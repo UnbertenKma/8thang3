6c6981b10a486d8de0804f44e250ae7b383bc50e
UnbertenKma <mvthang01@gmail.com>
2022-03-07 22:44:43 +0700

    upload

---
+++ config.js
@@ -0,0 +1,13 @@
+const nameGirl = 'Trangg';
+const giftUrl = 'http://nodemy.vn';
+const eventName = 'Chúc Mừng Ngày 8-3';
+const titleCard = 'Gửi Trangg';
+const contentCard = 'Chúc Em ngày 8/3 tràn ngập niềm vui và những nụ cười. Mong điều đẹp nhất sẽ đến với em trong hôm nay và cả những ngày sau .  <br> Chúc em tất cả !';
+
+// phần dưới dành cho các bạn biết code, nếu muốn chỉnh ảnh đơn giản với base64
+// Cần hỗ trợ hãy liên hệ: 
+// Mr-Nam http://facebook.com/nam.nodemy
+// Các bạn muốn học lập trình thì tham gia Nhóm zalo tự học lập trình nhé: https://zalo.me/g/yhdkef092
+const giftImage = 'hot-girl.jpg';
+const base64 = '';
+const giftImageBase64 = "data:image/jpg;base64, " + base64;
